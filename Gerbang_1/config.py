f3da68a77bdfa75b23bc19666f7e656a24f063d1
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-10-15 13:55:48 +0700

    membuat configigurasi IoT dan DB

---
+++ Gerbang_1/config.py
@@ -0,0 +1,20 @@
+
+# Konfigurasi Arduino
+PORT_ARDUINO = 'COM5'
+BAUDRATE = 9600
+
+# Konfigurasi Model & Kamera
+MODEL_PATH = 'plat.pt'
+SOURCE_PATH = 0
+TARGET_OCR_HEIGHT = 100
+
+# Konfigurasi Database
+DB_HOST = 'localhost'
+DB_USER = 'root'
+DB_PASSWORD = ''
+DB_NAME = 'mesin_parkir'
+DB_TABLE = 'data_mobil'
+
+# Konfigurasi Logika Deteksi
+CONFIRMATION_COUNT = 5
+DURASI_PENGUMPULAN = 4
